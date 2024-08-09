0d1e6d5c483ae50417b7b25d245eb22356ddf908
taka231 <taka23131@gmail.com>
2024-08-09 18:53:33 +0900

    feature: implemented the feature to read and execute file from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+
 use wasm_runtime::parser::Parser;
 use wasm_runtime::runtime::Runtime;
 
 fn main() {
-    let mut parser = Parser::new(include_bytes!("../tests/wasi/wasm_runtime.wasm"));
-    let sections = parser.parse().unwrap();
-    let mut runtime = Runtime::new(sections);
-    runtime.call_with_name("_start", Vec::new()).unwrap();
+    let mut filename = String::new();
+    print!("Enter the filename: ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut filename).unwrap();
+    let path = Path::new(".").join(filename.trim());
+    let mut file = File::open(path).unwrap();
+    let mut bytes = Vec::new();
+    file.read_to_end(&mut bytes).unwrap();
+    let mut parser = Parser::new(&bytes);
+    let module = parser.parse().unwrap();
+    let mut runtime = Runtime::new(module);
+    let result = runtime.call_with_name("_start", vec![]).unwrap();
+    println!("{:?}", result);
 }
