7b6fe4df4be82377f005c3eb669e54e7fe36a925
taka231 <taka23131@gmail.com>
2024-08-09 17:54:53 +0900

    feature: implemented random_get, fd_prestat_get, fd_prestat_dir_name and fd_close

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,8 @@ use wasm_runtime::parser::Parser;
 use wasm_runtime::runtime::Runtime;
 
 fn main() {
-    // (1)
-    let bytes = include_bytes!("../tests/wasm/sample.wasm");
-    let mut parser = Parser::new(bytes);
+    let mut parser = Parser::new(include_bytes!("../tests/wasi/wasm_runtime.wasm"));
     let sections = parser.parse().unwrap();
-    println!("(1)");
-    // println!("{:?}", &sections);
     let mut runtime = Runtime::new(sections);
     runtime.call_with_name("_start", Vec::new()).unwrap();
-    // (4)
-    let add_bytes = include_bytes!("../tests/wasm/add.wasm");
-    let mut parser = Parser::new(add_bytes);
-    let sections = parser.parse().unwrap();
-    println!("(5)");
-    println!("{:?}", sections);
-    println!("(6)");
-    let mut runtime = Runtime::new(sections);
-    let result = runtime.call_with_name("_start", Vec::new());
-    println!("{:?}", result);
-    // (7)
-    // 1~10までの和を計算するsum.wasmを実行出来るようにする
-    let sum_bytes = include_bytes!("../tests/wasm/sum.wasm");
-    let mut parser = Parser::new(sum_bytes);
-    let sections = parser.parse().unwrap();
-    println!("(7)(5)");
-    println!("{:?}", sections);
-    println!("(7)(6)");
-    let mut runtime = Runtime::new(sections);
-    let result = runtime.call_with_name("_start", Vec::new());
-    println!("{:?}", result);
 }
--- src/runtime/wasi.rs
+++ src/runtime/wasi.rs
@@ -1,4 +1,9 @@
-use std::{fs::File, io::Write, os::fd::FromRawFd};
+use std::{
+    env::{self},
+    fs::File,
+    io::Write,
+    os::fd::FromRawFd,
+};
 
 use super::{
     importer::Importer,
@@ -8,18 +13,23 @@ use super::{
 
 #[derive(Debug)]
 pub struct WasiSnapshotPreview1 {
-    pub file_table: Vec<File>,
+    pub file_table: Vec<Option<File>>,
+    pub file_path: Vec<Option<String>>,
 }
 
 impl WasiSnapshotPreview1 {
     pub fn new() -> Self {
+        let current_dir = File::open(".").unwrap();
+        let current_dir_path = env::current_dir().unwrap().to_str().map(|s| s.to_owned());
         unsafe {
             Self {
                 file_table: vec![
-                    File::from_raw_fd(0),
-                    File::from_raw_fd(1),
-                    File::from_raw_fd(2),
+                    Some(File::from_raw_fd(0)),
+                    Some(File::from_raw_fd(1)),
+                    Some(File::from_raw_fd(2)),
+                    Some(current_dir),
                 ],
+                file_path: vec![None, None, None, current_dir_path],
             }
         }
     }
@@ -29,7 +39,12 @@ impl WasiSnapshotPreview1 {
         let iovs_len = args[2].as_i32()?;
         let rp = args[3].as_i32()?;
 
-        let file = &mut self.file_table.get_mut(fd as usize).ok_or("Not found fd")?;
+        let file = self
+            .file_table
+            .get_mut(fd as usize)
+            .ok_or("Not found fd")?
+            .as_mut()
+            .ok_or("Not found fd")?;
 
         let mut nwritten = 0;
         let memory = &mut store.memory;
@@ -48,6 +63,56 @@ impl WasiSnapshotPreview1 {
 
         Ok(Value::I32(0))
     }
+
+    fn random_get(&mut self, store: &mut Store, args: Vec<Value>) -> Result<Value, String> {
+        let buf = args[0].as_i32()? as usize;
+        let buf_len = args[1].as_i32()? as usize;
+        for i in 0..buf_len {
+            let random = rand::random();
+            store.memory.data[buf + i] = random;
+        }
+        Ok(Value::I32(0))
+    }
+
+    fn fd_prestat_get(&mut self, store: &mut Store, args: Vec<Value>) -> Result<Value, String> {
+        let fd = args[0].as_i32()?;
+        let buf = args[1].as_i32()? as u32;
+
+        let Some(Some(path)) = self.file_path.get(fd as usize) else {
+            return Ok(Self::ERRNO_BADF.into());
+        };
+        store.memory.store(buf as u32, 0, 1, &[0])?;
+        store
+            .memory
+            .store(buf, 4, 4, &(path.len() as i32).to_le_bytes())?;
+        Ok(Value::I32(0))
+    }
+
+    fn fd_prestat_dir_name(
+        &mut self,
+        store: &mut Store,
+        args: Vec<Value>,
+    ) -> Result<Value, String> {
+        let fd = args[0].as_i32()? as usize;
+        let buf = args[1].as_i32()? as usize;
+
+        let Some(Some(path)) = self.file_path.get(fd as usize) else {
+            return Ok(Self::ERRNO_BADF.into());
+        };
+        for i in 0..path.len() {
+            store.memory.data[buf + i] = path.as_bytes()[i];
+        }
+        Ok(Value::I32(0))
+    }
+
+    fn fd_close(&mut self, args: Vec<Value>) -> Result<Value, String> {
+        let fd = args[0].as_i32()? as usize;
+        self.file_table[fd as usize] = None;
+        self.file_path[fd as usize] = None;
+        Ok(Value::I32(0))
+    }
+
+    const ERRNO_BADF: i32 = 8;
 }
 
 fn memory_read_4byte(memory: &Memory, addr: u32) -> Result<i32, String> {
@@ -59,6 +124,10 @@ impl Importer for WasiSnapshotPreview1 {
     fn call(&mut self, store: &mut Store, name: &str, args: Vec<Value>) -> Result<Value, String> {
         match name {
             "fd_write" => self.fd_write(store, args),
+            "random_get" => self.random_get(store, args),
+            "fd_prestat_get" => self.fd_prestat_get(store, args),
+            "fd_prestat_dir_name" => self.fd_prestat_dir_name(store, args),
+            "fd_close" => self.fd_close(args),
             _ => unimplemented!("{name}"),
         }
     }
